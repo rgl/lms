96e878dd0021e2e1e837bbfa426374e6d72abf96
RikiSharon <riki.sharon@intel.com>
2025-07-20 15:06:57 +0300

    LMS: PFW: Add missing m_serviceIsClosed
    Signed-off-by: Riki Sharon <riki.sharon@intel.com>

--- UNS/PortForwardingService/PortForwardingService.cpp
+++ UNS/PortForwardingService/PortForwardingService.cpp
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (C) 2010-2023 Intel Corporation
+ * Copyright (C) 2010-2025 Intel Corporation
  */
 #include "PortForwardingService.h"
 
@@ -439,6 +439,7 @@ void PortForwardingService::OnStop()
 	}
 	m_mainService->SetHeciEventCB(NULL, NULL, NULL);
 	closeSubService();
+	m_serviceIsClosed = true;
 }
 
 
